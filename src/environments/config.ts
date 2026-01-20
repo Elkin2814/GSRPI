e660db63d5a502072deec47016c8ace654a6b196
ParraJesus <jgparra@unicauca.edu.co>
2026-01-19 21:01:55 -0500

    another typo fix

--- src/environments/config.ts
+++ src/environments/config.ts
@@ -1,6 +1,6 @@
 import { environment } from "./environment";
 
-const base_url_api_gsrpi = ENVIRONMENT.BASE_URL_API_GSRPI;
+const base_url_api_gsrpi = environment.apiUrl;
 
 const ENVIRONMENTS = {
 
@@ -8,9 +8,6 @@ const ENVIRONMENTS = {
     POST_LOGIN: `${base_url_api_gsrpi}/auth/login`,
     POST_LOGOUT: `${base_url_api_gsrpi}/auth/logout`,
     POST_REFRESH_TOKEN: `${base_url_api_gsrpi}/auth/refresh-token`,
-    //Supabase auth
-    BASE_URL_SUPABASE: ENVIRONMENT.BASE_URL_SUPABASE,
-    PUBLIC_API_KEY_SUPABASE: ENVIRONMENT.PUBLIC_API_KEY_SUPABASE,
 
     //USER MANAGEMENT
     GET_USERS_PAGINATED: `${base_url_api_gsrpi}/obtener-usuarios-paginados`,
